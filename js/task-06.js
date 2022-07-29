56a752b5c166d86c98595ef59849580d3a5b4812
Aleksandr-Alekseenko-23 <alekseenko48@bigmir.net>
2022-07-29 17:15:03 +0300

    add2

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,15 @@
-const textInput = document.querySelector("#validation-input");
+let textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener("blur", (event) => {
   if (event.target.value.length === Number(event.target.dataset.length)) {
     textInput.classList.add("valid");
     textInput.classList.remove("invalid");
   } else {
+    switchClass();
+    console.log(switchClass);
+  }
+
+  function switchClass(valid, invalid) {
     textInput.classList.add("invalid");
     textInput.classList.remove("valid");
   }
